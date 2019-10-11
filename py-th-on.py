aa0a25cff4396f070f0b63cd841f88cba31c1977
jgeorg0001 <jgeorg0001@gmail.com>
2019-10-11 20:06:59 +0530

    Feature (#3)
    * second but extended
    
    * py-ft1
    
    * py-mas1
    
    * f2
    
    * j1
    
    * j2

---
+++ py-th-on.py
@@ -0,0 +1,15 @@
+#! /ust/bin/python3
+import os,sys,re
+#import subprocess
+
+def f2(y):
+    y = y * 2
+    return y
+
+def f1(x):
+    x = 5
+    y = f2(x)
+    return y
+
+if __name__ == "__main__":
+   k = f1(x)
